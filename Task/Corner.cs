e25194a661a4ba5110b3170eb6ff77e124749cd9
Sergei Prutsakov <psn206@mail.ru>
2024-04-19 14:37:26 +0300

    Добавьте файлы проекта.

---
+++ Task/Corner.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    class Corner
+    {
+        int gradus;
+        int min;
+        int sec;
+
+       
+        public int Gradus
+        {
+            get { return gradus; }
+            set
+            {
+
+                gradus = value > 360 ? value % 360 : value;
+            }
+        }
+        public int Min
+        {
+            get { return min; }
+            set
+            {
+             
+                if (value > 60)
+                {
+                    Gradus += value / 60;
+                    min = value % 60;
+                }
+                else { min = value; }
+            }
+        }
+
+        public int Sec
+        {
+            get { return sec; }
+            set
+            {
+                if (value > 60)
+                {
+                    Min += value / 60;
+                    sec = value % 60;
+                }
+                else
+                {
+                    sec = value;
+                }
+            }
+        }
+
+        public double GetRadians()
+        {
+            return (1 / (Math.PI * 180.0)) * ((double)gradus + min / 60.0 + sec / 3600.0);
+        }
+    }
+}
---
+++ Task/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Task;
+
+
+namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите градусы: ");
+            int gradus = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите минуты: ");
+            int min = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите секунды: ");
+            int sec = Convert.ToInt32(Console.ReadLine());
+            Corner corner = new Corner();
+            corner.Gradus = gradus;
+            corner.Min = min;
+            corner.Sec = sec;
+
+            Console.WriteLine($"{corner.Gradus} {corner.Min} {corner.Sec} ");
+            Console.WriteLine($"Рыдианы = {corner.GetRadians()}");
+            Console.ReadKey();
+        }
+    }
+
+
+}
